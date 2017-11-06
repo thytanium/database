1307ffadb4148218f63f1cdd8855729407c81192
Alejandro González <thytanium@gmail.com>
2017-11-05 22:46:51 -0400

    Testing Dropdown::build() individually instead of in dropdown() scope

--- tests/Unit/Eloquent/Traits/DropdownableTest.php
+++ tests/Unit/Eloquent/Traits/DropdownableTest.php
@@ -29,87 +29,22 @@ class DropdownableTest extends TestCase
      */
     protected function fillDb()
     {
-        TestModel::create(['name' => 'model-1', 'email' => 'email@example.com']);
-        TestModel::create(['name' => 'model-2', 'email' => 'email2@example.com']);
+        TestModel::create(['name' => 'model-X', 'email' => 'email@example.com']);
+        TestModel::create(['name' => 'model-Y', 'email' => 'email2@example.com']);
     }
 
     /**
-     * Test most basic case.
+     * Test dropdown() scope with basic params.
      * 
      * @return void
      */
-    public function test_basic_case()
+    public function test_dropdown_scope()
     {
         $result = TestModel::dropdown();
 
         $this->assertEquals([
-            1 => 'model-1',
-            2 => 'model-2',
-        ], $result->toArray());
-    }
-
-    /**
-     * Test with different column names.
-     * 
-     * @return void
-     */
-    public function test_with_other_columns()
-    {
-        $result = TestModel::dropdown('email', 'name');
-
-        $this->assertEquals([
-            'email@example.com' => 'model-1',
-            'email2@example.com' => 'model-2',
-        ], $result->toArray());
-
-        $result = TestModel::dropdown('name', 'email');
-
-        $this->assertEquals([
-            'model-1' => 'email@example.com',
-            'model-2' => 'email2@example.com',
-        ], $result->toArray());
-    }
-
-    /**
-     * Test with callables as column names.
-     * 
-     * @return void
-     */
-    public function test_with_callables()
-    {
-        $result = TestModel::dropdown(function ($model) {
-            return "{$model->id}.{$model->email}";
-        }, 'name');
-
-        $this->assertEquals([
-            '1.email@example.com' => 'model-1',
-            '2.email2@example.com' => 'model-2',
-        ], $result->toArray());
-
-        $result = TestModel::dropdown('id', function ($model) {
-            return "{$model->email} : {$model->name}";
-        });
-
-        $this->assertEquals([
-            1 => 'email@example.com : model-1',
-            2 => 'email2@example.com : model-2',
-        ], $result->toArray());
-    }
-
-    /**
-     * Test with callables as column names with index.
-     * 
-     * @return void
-     */
-    public function test_callable_with_index()
-    {
-        $result = TestModel::dropdown(function ($model, $index) {
-            return $index;
-        });
-
-        $this->assertEquals([
-            0 => 'model-1',
-            1 => 'model-2',
+            1 => 'model-X',
+            2 => 'model-Y',
         ], $result->toArray());
     }
 }
--- tests/Unit/Util/DropdownTest.php
+++ tests/Unit/Util/DropdownTest.php
@@ -15,12 +15,108 @@ class DropdownTest extends TestCase
      * 
      * @return Dropdown
      */
-    protected function buildDropdown()
+    protected function dropdown()
     {
         return new Dropdown([
             1 => 'model-1',
             2 => 'model-2',
         ]);
+    }
+
+    /**
+     * Data for testing.
+     *
+     * @param  array $params
+     * @return array
+     */
+    protected function dropdownBuilder($params = [])
+    {
+        $data = [
+            (object) ['id' => 1, 'name' => 'model-1', 'email' => 'some_email@thytanium.com'],
+            (object) ['id' => 2, 'name' => 'model-2', 'email' => 'some_other_email@thytanium.com'],
+        ];
+
+        return call_user_func_array('Thytanium\Database\Util\Dropdown::build', Arr::prepend($params, $data));
+    }
+
+    /**
+     * Test the most basic case.
+     * 
+     * @return void
+     */
+    public function test_basic_case()
+    {
+        $result = $this->dropdownBuilder();
+
+        $this->assertEquals([
+            1 => 'model-1',
+            2 => 'model-2',
+        ], $result->toArray());
+    }
+
+    /**
+     * Test with different column names.
+     * 
+     * @return void
+     */
+    public function test_with_other_columns()
+    {
+        $result = $this->dropdownBuilder(['email', 'name']);
+
+        $this->assertEquals([
+            'some_email@thytanium.com' => 'model-1',
+            'some_other_email@thytanium.com' => 'model-2',
+        ], $result->toArray());
+
+        $result = $this->dropdownBuilder(['name', 'email']);
+
+        $this->assertEquals([
+            'model-1' => 'some_email@thytanium.com',
+            'model-2' => 'some_other_email@thytanium.com',
+        ], $result->toArray());
+    }
+
+    /**
+     * Test with callables as column names.
+     * 
+     * @return void
+     */
+    public function test_with_callables()
+    {
+        $result = $this->dropdownBuilder([function ($model) {
+            return "{$model->id}.{$model->email}";
+        }, 'name']);
+
+        $this->assertEquals([
+            '1.some_email@thytanium.com' => 'model-1',
+            '2.some_other_email@thytanium.com' => 'model-2',
+        ], $result->toArray());
+
+        $result = $this->dropdownBuilder(['id', function ($model) {
+            return "{$model->email} : {$model->name}";
+        }]);
+
+        $this->assertEquals([
+            1 => 'some_email@thytanium.com : model-1',
+            2 => 'some_other_email@thytanium.com : model-2',
+        ], $result->toArray());
+    }
+
+    /**
+     * Test with callables as column names with index.
+     * 
+     * @return void
+     */
+    public function test_callable_with_index()
+    {
+        $result = $this->dropdownBuilder([function ($model, $index) {
+            return $index;
+        }]);
+
+        $this->assertEquals([
+            0 => 'model-1',
+            1 => 'model-2',
+        ], $result->toArray());
     }
 
     /**
@@ -30,7 +126,7 @@ class DropdownTest extends TestCase
      */
     public function test_array_access()
     {
-        $dropdown = $this->buildDropdown();
+        $dropdown = $this->dropdown();
 
         $this->assertArrayNotHasKey(0, $dropdown);
         $this->assertCount(2, $dropdown);
@@ -66,7 +162,7 @@ class DropdownTest extends TestCase
      */
     public function test_arrayable()
     {
-        $dropdown = $this->buildDropdown();
+        $dropdown = $this->dropdown();
 
         $this->assertEquals([
             1 => 'model-1',
@@ -81,7 +177,7 @@ class DropdownTest extends TestCase
      */
     public function test_append()
     {
-        $dropdown = $this->buildDropdown();
+        $dropdown = $this->dropdown();
 
         $dropdown->append('model-x');
 
@@ -101,7 +197,7 @@ class DropdownTest extends TestCase
      */
     public function test_prepend()
     {
-        $dropdown = $this->buildDropdown();
+        $dropdown = $this->dropdown();
 
         $dropdown->prepend('model-x');
 
@@ -124,7 +220,7 @@ class DropdownTest extends TestCase
         // Translation namespace
         $namespace = 'namespace';
 
-        $dropdown = $this->buildDropdown();
+        $dropdown = $this->dropdown();
 
         $translator = m::mock('Illuminate\Translation\Translator[has,get]', [new ArrayLoader, 'xx']);
 
